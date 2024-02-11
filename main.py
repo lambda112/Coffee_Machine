6c78277e9d64751f28a0484a96e99797f6f9f92d
Kamran Azim <Kamazim121212@gmail.com>
2024-02-11 20:40:51 +0000

    Completed 'Allow machine to be turned off with the keyword off' and 'Allow user to enter keyword report, which will generate a report that shows current rescource values'

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from rescource_data import MENU, resources
-
+import os
 
 # Ask the user what flavour they want return flavour type
+# Allow machine to be turned off with the keyword 'off' 
+# Allow user to enter keyword report, which will generate a report that shows current rescource values
 
 def get_order():
     coffee_type = input("What would you like? (espresso, latte, cappuccino): ")
     while coffee_type not in MENU.keys():
-        coffee_type = input("Enter valid order! (espresso, latte, cappuccino): ")
+
+        if coffee_type == "report":
+            print(f"Coffee Machine Rescources: {resources}\n")
+            
+        elif coffee_type == "off":
+            print("Machine Powering Off!")
+            os._exit(200)
+
+        coffee_type = input("What would you like? (espresso, latte, cappuccino): ")
 
     coffee_type = MENU[coffee_type]
     return coffee_type
-
-
-# Allow machine to be turned off with the keyword 'off' 
-
-
-# Allow user to enter keyword report, which will generate a report that shows current rescource values
-
 
 # Check if there are enough rescources
 
