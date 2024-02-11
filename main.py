72bb69d3c3d862ff2df226f524cbb1976e4e1283
Kamran Azim <Kamazim121212@gmail.com>
2024-02-11 22:51:09 +0000

    Finsished Program
    Program ends if there is not enough rescources

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from rescource_data import MENU, resources
+from resource_data import MENU, resources
 import os
 
-# Ask the user what flavour they want return flavour type
-# Allow machine to be turned off with the keyword 'off' 
-# Allow user to enter keyword report, which will generate a report that shows current rescource values
-def get_order():
+
+def get_order() -> dict:
+    """Ask the user what flavour they want and then return flavour type.
+    Allow machine to be turned off with the keyword 'off'.
+    Allow user to enter keyword report, which will generate a report that shows current rescource values"""
+
     coffee_type = input("What would you like? (espresso, latte, cappuccino): ")
     while coffee_type not in MENU.keys():
 
@@ -22,33 +24,71 @@ def get_order():
 
 
 
-# Check if there are enough rescources
-def check_rescources(): 
-    order = get_order()["ingredients"]
+def check_rescources(order: dict, deduct: bool = False) -> bool:
+    """Check if there are enough resources to make coffee. Returns either true or false based on result. 
+    Can provide deduct boolean to deduct from resources"""
 
+    order = order["ingredients"]
     for i in order:
+
         if resources[i] < order[i]:
             print(f"Sorry not enough {i}!") 
-        else:
+            return False
+
+        elif deduct:
             resources[i] -= order[i]
 
-    return resources
+    return True
 
 
-# Process coins 
-def enter_coins():
+
+def enter_coins() -> float:
+    """Allows user to enter money in the form of coins. Returns total of coins"""
+
     while True:
         try:
             pennies = (int(input("How many pennies?: ")) / 100)
             nickles = (int(input("How many nickles?: ")) / 100) * 5
             dimes = (int(input("How many dimes?: ")) / 100) * 10 
             quarters = (int(input("How many quarters?: ")) / 100) * 25
-            amount = pennies + nickles + dimes + quarters
-            return amount
+
+            coin_amount = pennies + nickles + dimes + quarters
+            return coin_amount
 
         except ValueError:
             print("Please enter a valid amount!\n")       
             
 
-print(enter_coins())
-# Check if transaction is successful
+
+
+            
+def transaction(order: dict, amount) -> bool:
+    """Check if transaction is successful. Returns result of check rescources or prints message if not enough money"""
+
+    if order["cost"] > amount: 
+        print("Sorry that's not enough money. Money refunded")
+
+    else:
+        print(f"Here is your ${round(amount - order['cost'], 2)} change!")
+        try:
+            resources["Money"] += order["cost"] 
+        except KeyError:
+            resources["Money"] = order["cost"] 
+
+        return check_rescources(order, deduct=True)
+
+
+
+def main():
+
+    while True:
+        customer_order = get_order()
+        if not check_rescources(customer_order, deduct = False):
+            break
+
+        customer_amount = enter_coins()
+        transaction(customer_order, customer_amount)
+
+
+main()
+
